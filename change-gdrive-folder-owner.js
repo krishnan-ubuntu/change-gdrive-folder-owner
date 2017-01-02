d165afb25c017c91b20cfadb0e1febc9cf40ca52
Krishnan <krishnan.ubuntu@gmail.com>
2017-01-02 23:10:05 +0530

    minor changes

--- change-gdrive-folder-owner.js
+++ change-gdrive-folder-owner.js
@@ -1,5 +1,15 @@
 function changeFolderOwner()
 {
-  var folder = DriveApp.getFoldersByName('Some Folder');
-  folder.setOwner("user@domain.com");
+  var today = new Date();
+  var MS_PER_MINUTE = 60000;
+  var myStartDate = new Date(today - 5 * MS_PER_MINUTE);
+  
+  var folders = DriveApp.getFoldersByName('Test').next();
+  var fileSearchQuery = 'modifiedDate > "'+myStartDate.toISOString()+'"';
+  var files = folders.searchFiles(fileSearchQuery);
+  while (files.hasNext())
+  {
+     file = files.next();
+     file.setOwner('user@domain.com');     
+   }
 }
